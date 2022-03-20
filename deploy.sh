4b3dc4ae3c55e046891938c2e16547a95c08ad93
Aman <amanfromoakland@gmail.com>
2022-03-19 22:23:55 -0400

    feat: Install Composer and amqplib dependencies

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ sudo apt full-upgrade -y
 sudo apt autoremove -y --purge
 
 # Install required packages
-sudo apt install -y ufw php-amqp php-bcmath php-cli php-common php-curl php-fpm php-json php-mbstring php-mysql php-readline php-opcache php-readline nginx
+sudo apt install -y ufw php-amqp php-bcmath php-cli php-common php-curl php-fpm php-json php-mbstring php-mysql php-readline php-opcache php-readline php-zip nginx wget unzip
 
 # Setup firewall
 sudo ufw --force enable
@@ -23,6 +23,12 @@ sudo ufw default allow outgoing
 # Install zerotier
 sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
 curl -s https://install.zerotier.com | sudo bash
+
+# Install Composer
+sudo wget -O composer-setup.php https://getcomposer.org/installer
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer require php-amqplib/php-amqplib
+composer update
 
 # Stop nginx
 sudo systemctl stop nginx
